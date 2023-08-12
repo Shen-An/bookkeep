8cd61764aedfefd490a412c5df3abd3c2ab98a90
Ericple Garrison <dev@peercat.cn>
2023-08-12 16:30:35 +0800

    初始提交

---
+++ entry/src/main/ets/data/defaultBalanceTemplate.ts
@@ -0,0 +1,161 @@
+export const advancedTemplate = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  []
+]
+
+export const BalanceTemplate: {day: number, balances: {remark: string, amount: string, type: string}[]}[] = [
+  {
+    day: 1,
+    balances: []
+  },
+  {
+    day: 2,
+    balances: []
+  },
+  {
+    day: 3,
+    balances: []
+  },
+  {
+    day: 4,
+    balances: []
+  },
+  {
+    day: 5,
+    balances: []
+  },
+  {
+    day: 6,
+    balances: []
+  },
+  {
+    day: 7,
+    balances: []
+  },
+  {
+    day: 8,
+    balances: []
+  },
+  {
+    day: 9,
+    balances: []
+  },
+  {
+    day: 10,
+    balances: []
+  },
+  {
+    day: 11,
+    balances: []
+  },
+  {
+    day: 12,
+    balances: []
+  },
+  {
+    day: 13,
+    balances: []
+  },
+  {
+    day: 14,
+    balances: []
+  },
+  {
+    day: 15,
+    balances: []
+  },
+  {
+    day: 16,
+    balances: []
+  },
+  {
+    day: 17,
+    balances: []
+  },
+  {
+    day: 18,
+    balances: []
+  },
+  {
+    day: 19,
+    balances: []
+  },
+  {
+    day: 20,
+    balances: []
+  },
+  {
+    day: 21,
+    balances: []
+  },
+  {
+    day: 22,
+    balances: []
+  },
+  {
+    day: 23,
+    balances: []
+  },
+  {
+    day: 24,
+    balances: []
+  },
+  {
+    day: 25,
+    balances: []
+  },
+  {
+    day: 26,
+    balances: []
+  },
+  {
+    day: 27,
+    balances: []
+  },
+  {
+    day: 28,
+    balances: []
+  },
+  {
+    day: 29,
+    balances: []
+  },
+  {
+    day: 30,
+    balances: []
+  },
+  {
+    day: 31,
+    balances: []
+  },
+]
